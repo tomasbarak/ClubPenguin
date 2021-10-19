b4bc7a2b6fafdfee749c9dfbb3962ab258417db4
Tomás Barak <70740123+tomasbarak@users.noreply.github.com>
2021-10-19 08:17:32 -0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ClubPenguin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClubPenguin.dir/functions.cpp.o"
+  "CMakeFiles/ClubPenguin.dir/main.cpp.o"
+  "ClubPenguin"
+  "ClubPenguin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClubPenguin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
